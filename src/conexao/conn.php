b004c3b292df67b10d4d69d6013e2d6c5e8a52ef
Rennan <rennan.guilherme63@gmail.com>
2022-01-05 19:35:27 -0300

    04/01

--- src/conexao/conn.php
+++ src/conexao/conn.php
@@ -1,15 +1,15 @@
 <?php
 
     // Declarar as variáveis necessárias para a conexão com o banco de dados........
-    $hostname = "sql211.epizy.com"; //Nome do servidor que se encontra nosso banco de dados
-    $dbname = "epiz_30729343_CANTINA"; //Nome do nosso banco de dados
-    $username = "epiz_30729343"; //Nome do usuário para acesso ao banco de daddos
-    $password = "ASEBUnxIylHMYm"; //Senha de acesso ao nosso banco de daddos
+    $hostname = "sql304.epizy.com"; //Nome do servidor que se encontra nosso banco de dados
+    $dbname = "epiz_30728937_CANTINA"; //Nome do nosso banco de dados
+    $username = "epiz_30728937"; //Nome do usuário para acesso ao banco de daddos
+    $password = "k3LFZYKXIJ"; //Senha de acesso ao nosso banco de daddos
 
     try {
-        $pdo = new PDO('mysql:host='.$hostname.';dbaname='.$dbname, $username, $password);
+        $pdo = new PDO('mysql:host='.$hostname.';dbname='.$dbname, $username, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo 'Conexão com banco de dados, realizado com sucesso!!!';
+        // echo 'Conexão com banco de dados, realizado com sucesso!!!';
     }catch(PDOException $e) {
         echo 'Erro: '.$e->getMessage();
     }
